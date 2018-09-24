c2772f7868b91806007618940c6dee6aa2fc27e3
limbowandering <591176276@qq.com>
2018-09-24 23:05:10 +0800

    update queue;

---
+++ basic-methods/queue/queue.js
@@ -0,0 +1,16 @@
+function Queue(){
+  let items = [];
+  this.enqueue = function(element){
+    items.push(element);
+  };
+  this.dequeue = function(element){
+    return items.shift();
+  };
+  this.front = function(){
+    return items[0];
+  };
+  this.isEmpty = function(){
+    return items.length === 0;
+  };
+  
+}
