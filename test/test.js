c25f7e54b35b1d069469c2b58346866ea874a3ca
limbowandering <591176276@qq.com>
2018-09-28 23:46:54 +0800

    今天不该打游戏的;

--- test/test.js
+++ test/test.js
@@ -15,9 +15,28 @@ function Queue(){
   
 }
 
-const plusOne = function(digits){
+const plusOne1 = function(digits){
   return (parseInt(digits.join(''))+1).toString().split('').map(e=> parseInt(e));
+}
+
+const plusOne = function(digits){
+  
+  let d = parseInt(digits[digits.length-1]);
+  if(d === 9){
+    let arr = digits.slice();
+    let t1 = arr.splice(arr.length-1,1);
+    let t2 = arr.splice(arr.length-1,1);
+    let t = parseInt(t2+t1)+1;
+    let str = arr.join('');
+    return (str+t).split('').map(e=>parseInt(e));
+  }else{
+    let arr = digits.slice();
+    let t = parseInt(arr.splice(arr.length-1,1))+1;
+    let str = arr.join('');
+    return (str+t).split('').map(e=>parseInt(e));
+  }
 }
 
 // console.log(plusOne([1,2,3]));
 console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]));
+console.log(plusOne([2,9]));
