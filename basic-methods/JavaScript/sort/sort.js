0d5210f1c21eeef2663345bd605b458993f7bc8d
limbowandering <591176276@qq.com>
2018-10-17 09:49:00 +0800

    update sort

--- basic-methods/JavaScript/sort/sort.js
+++ basic-methods/JavaScript/sort/sort.js
@@ -2,7 +2,7 @@ function ArrayList(){
     let array = [];
 
     this.insert = function(item){
-        array.item(item);
+        array.push(item);
     };
 
     const swap = function(array, index1, index2){
@@ -34,3 +34,46 @@ function ArrayList(){
         }
     }
 }
+
+function createNonSortedArray(size){
+    var array = new ArrayList();
+
+    for (var i = size; i> 0; i--){
+        array.insert(i);
+    }
+
+    return array;
+}
+
+function createRandomNonSortedArray(){
+    var array = new ArrayList();
+
+    array.insert(3);
+    array.insert(5);
+    array.insert(1);
+    array.insert(6);
+    array.insert(4);
+    array.insert(7);
+    array.insert(2);
+
+    return array;
+}
+
+function printArray(array){
+    console.log(array.toString());
+}
+
+function createNonSortedArrayAndPrint(size){
+    var array = createNonSortedArray(size);
+    printArray(array);
+
+    return array;
+}
+
+console.log('********** Bubble Sort **********');
+
+var array = createNonSortedArrayAndPrint(5);
+
+array.bubbleSort();
+
+printArray(array);
