7a527b38ec161bb63dd3be8df453c61c9eae919e
limbowandering <591176276@qq.com>
2018-10-22 21:18:05 +0800

    update 461

---
+++ 400-500/461. Hamming Distance/461. Hamming Distance.js
@@ -0,0 +1,11 @@
+const hammingDistance = function(x,y){
+  let a = x ^ y;
+  a = a.toString(2);
+  let res = 0;
+  for(let i = 0 ; i < a.length; i++){
+    res += a[i] === '1';
+  }
+  return res;
+}
+
+console.log(hammingDistance(1,4));
