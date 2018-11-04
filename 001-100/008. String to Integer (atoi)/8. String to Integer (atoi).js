3acc71eaa670abb244f35ef074230acd26ff8620
limbowandering <591176276@qq.com>
2018-11-04 12:55:36 +0800

    update 008

--- 001-100/008. String to Integer (atoi)/8. String to Integer (atoi).js
+++ 001-100/008. String to Integer (atoi)/8. String to Integer (atoi).js
@@ -1,4 +1,4 @@
-const myAtoi = function(str){
+const myAtoi1 = function(str){
   let isNegative = false;
   let notFirstChar = true;
   let res = '';
@@ -59,6 +59,34 @@ const myAtoi = function(str){
     }
   }
   return 0;
+}
+
+const myAtoi2 = function(str){
+  return Math.max(Math.min(parseInt(str) || 0, 2147483647), -2147483648);
+}
+
+const myAtoi = function(str){
+  str = str.trim();
+  if(!str) return 0;
+  let isPositive = 1;
+  let i = 0, ans = 0;
+  if(str[i] === '+'){
+    isPositive = 1;
+    i++;
+  }else if(str[i] === '-'){
+    isPositive = 0;
+    i++;
+  }
+  for(; i < str.length; i++){
+    let t = str.charCodeAt(i) - 48;
+    if(t > 9 || t < 0) break;
+    if(ans > 2147483647/10 || ans > (2147483647-t)/10){
+      return isPositive ? 2147483647 : -2147483648;
+    }else{
+      ans = ans*10 + t;
+    }
+  }
+  return isPositive? ans : -ans;
 }
 
 console.log(myAtoi('42'));
