f7591bc87d06bdf6fd376fe851de006011a3a9fe
limbowandering <591176276@qq.com>
2018-11-04 14:37:56 +0800

    update

--- 201-300/283. Move Zeroes/283. Move Zeroes.js
+++ 201-300/283. Move Zeroes/283. Move Zeroes.js
@@ -1,13 +1,4 @@
-const moveZeros2 = function(nums){
-  for(let i = 0; i < nums.length;i++){
-    if(nums[i] === 0){
-      let t = nums.splice(i,1);
-      nums.push(...t);
-      i--;
-    }
-  }
-  console.log(nums);
-}
+
 
 const moveZeros = function(nums){
   for(let i = 0; i < nums.length; i++){
