2daf4b61d07c4bca76dbf77fbae4010bc65d7188
limbowandering <591176276@qq.com>
2019-04-25 23:06:51 +0800

    update

---
+++ 001-100/058. Length of Last Word/58. Length of Last Word.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  s = s.trim()
+  return s.length - s.lastIndexOf(" ") - 1
+};
+
+var lengthOfLastWord2 = function (s) {
+  let res = 0
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== ' ' && (i === 0 || s[i - 1] === ' ')) {
+      res = 1
+    } else if (s[i] !== ' ') {
+      res++
+    }
+  }
+  return res
+}
+
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord('a'))
+console.log(lengthOfLastWord(' a '))
---
+++ 201-300/234. Palindrome Linked List/234. Palindrome Linked List.js
@@ -0,0 +1,86 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+const linkedList = require('../basic-methods/JavaScript/LinkedList/linkedListLeetcode');
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var isPalindrome2 = function(head) {
+    if(!head || !head.next) {
+      return true
+    }
+    let slow = head
+    let fast = head
+    while (fast !== null && fast.next !== null) {
+      slow = slow.next
+      fast = fast.next.next
+    }
+    if (fast !== null) {
+      slow = slow.next
+    }
+
+    let p = new ListNode(0)
+    while (slow !== null) {
+      let t = slow.next
+      slow.next = p.next
+      p.next = slow
+      slow = t
+    }
+
+    let left = head
+    let right = p.next 
+    while (right !== null) {
+      if (left.val !== right.val) {
+        return false
+      }
+      left = left.next
+      right = right.next
+    }
+    return true
+};
+
+var isPalindrom3 = function(head) {
+  let arr = [];
+  let next = head;
+  while (next) {
+    arr.push(next.val);
+    next = next.next;
+  }
+  let len = Math.trunc(arr.length / 2);
+  for (let i=0; i<len; i++) {
+    if (arr[i] !== arr[arr.length-1-i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+var isPalindrome = function(head) {
+  let arr = []
+  while(head) {
+    arr.push(head.val)
+    head = head.next
+  }
+  return arr.join('') === arr.reverse().join('')
+}
+
+let a = new linkedList.LinkedList();
+a.append(1)
+a.append(2)
+a.append(2)
+a.append(1)
+
+a.print()
+console.log(isPalindrome(a.getHead()))
